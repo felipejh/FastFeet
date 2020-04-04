d8a230c73480d446e492359d8af901993c506f09
Felipe Hoffmann <felipejh@gmail.com>
2020-04-04 18:10:04 -0300

    First commit

---
+++ web/src/components/PaginationButtons/index.js
@@ -0,0 +1,41 @@
+﻿import React from 'react';
+import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md';
+
+import PropTypes from 'prop-types';
+
+import { Container } from './styles';
+
+const propTypes = {
+  page: PropTypes.func.isRequired,
+  renderPrev: PropTypes.bool.isRequired,
+  renderNext: PropTypes.bool.isRequired,
+  handlePrevPage: PropTypes.func.isRequired,
+  handleNextPage: PropTypes.func.isRequired,
+};
+
+export default function PaginationButtons({
+  page,
+  renderPrev,
+  renderNext,
+  handlePrevPage,
+  handleNextPage,
+}) {
+  return (
+    <Container>
+      {renderPrev && (
+        <button type="button" onClick={handlePrevPage}>
+          <MdNavigateBefore size={30} color="#fff" />
+        </button>
+      )}
+
+      <strong>Página {page}</strong>
+      {renderNext && (
+        <button type="button" onClick={handleNextPage}>
+          <MdNavigateNext size={30} color="#fff" />
+        </button>
+      )}
+    </Container>
+  );
+}
+
+PaginationButtons.propTypes = propTypes;
---
+++ web/src/components/PaginationButtons/styles.js
@@ -0,0 +1,36 @@
+﻿import styled from 'styled-components';
+import { darken } from 'polished';
+
+import color from '~/styles/colors';
+
+export const Container = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  text-align: center;
+  border-radius: 4px;
+
+  button {
+    align-items: center;
+    margin: 15px;
+    justify-content: center;
+    height: 36px;
+    width: 36px;
+    background: ${color.primary};
+    border: 0;
+    border-radius: 4px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.03, color.primary)};
+    }
+  }
+
+  strong {
+    font-size: 14px;
+    color: #444;
+    margin-left: 5px;
+  }
+`;
